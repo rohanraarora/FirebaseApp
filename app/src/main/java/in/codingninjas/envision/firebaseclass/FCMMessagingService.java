7f800011975b8568e8b6c9dd8a84dc0b22f13434
Rohan Arora <rohanraarora@gmail.com>
2018-08-05 13:46:47 +0530

    Add cloud messaging

---
+++ app/src/main/java/in/codingninjas/envision/firebaseclass/FCMMessagingService.java
@@ -0,0 +1,28 @@
+package in.codingninjas.envision.firebaseclass;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FCMMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.d("NotificationMessage","Message received");
+
+        if (remoteMessage.getData().size() > 0) {
+            //Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            //Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+    }
+}
--- app/src/main/java/in/codingninjas/envision/firebaseclass/MainActivity.java
+++ app/src/main/java/in/codingninjas/envision/firebaseclass/MainActivity.java
@@ -1,21 +1,29 @@
 package in.codingninjas.envision.firebaseclass;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 import java.util.ArrayList;
 
@@ -32,15 +40,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         listView = findViewById(R.id.listview);
         adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,items);
         listView.setAdapter(adapter);
 
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        database.setPersistenceEnabled(true);
+
+        //database.setPersistenceEnabled(true);
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if(task.isSuccessful()){
+                    Log.d("Token",task.getResult().getToken());
+                }else {
+                    Log.e("Token",task.getException().getMessage());
+                }
+            }
+        });
         DatabaseReference rootRef = database.getReference();
-        expensesRef = rootRef.child("expenses");
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        String uid = user.getUid();
+        DatabaseReference uRef = rootRef.child("users").child(uid);
+        expensesRef = uRef.child("expenses");
 
         expensesRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -100,6 +125,13 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public void logout(View view){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     public void onButtonPressed(View view){
